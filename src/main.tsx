6d7789d382d62546263a4702e1684d81bf41f644
DiogoCalmon <diogocalmons@gmail.com>
2025-07-08 19:34:18 -0300

    adding routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './login/login.tsx'
+import Login from './login/login.tsx'
+import Registro from './registro/registro.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />
+  },
+  {
+    path: "/registro",
+    element: <Registro />
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
